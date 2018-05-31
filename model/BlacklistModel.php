201844e3e935fdea99217852b2654aba2957fe18
Sarah Carolina J. Silva <sarah_2812@msn.com>
2018-05-31 01:40:35 -0300

    Added PHPDoc function comments.

--- model/BlacklistModel.php
+++ model/BlacklistModel.php
@@ -10,6 +10,9 @@ class BlacklistModel
 {
     private $conn = null;
 
+    /**
+     * BlacklistModel constructor. Sets up connection to SQLite database.
+     */
     function __construct() {
         try {
             $this->conn = new PDO("sqlite: db.sqlite3");
@@ -18,6 +21,10 @@ class BlacklistModel
         }
     }
 
+    /**
+     * Function that retrieves the number of active CPFs in blacklist
+     * @return mixed|string
+     */
     function getCount() {
         try {
             $query = "SELECT COUNT(cpf) AS QTD FROM blacklist WHERE status = 1;";
@@ -28,7 +35,12 @@ class BlacklistModel
         }
     }
 
-    function checkStatus($cpf = null) {
+    /**
+     * Check if a CPF is considered in blacklist of not
+     * @param null $cpf
+     * @return string
+     */
+    function checkCpfStatus($cpf = null) {
         try {
             $query = "SELECT * FROM blacklist WHERE cpf = :cpf AND status = 1;";
             $stmt = $this->conn->prepare($query);
@@ -46,19 +58,28 @@ class BlacklistModel
         }
     }
 
-    function getHistory($cpf = null) {
+    /**
+     * Get all past appearances of a CPF in blacklist
+     * @param null $cpf
+     * @return string
+     */
+    function getCpfHistory($cpf = null) {
         try {
             $query = "SELECT * FROM blacklist WHERE cpf = :cpf ORDER BY datahora_inclusao DESC";
             $stmt = $this->conn->prepare($query);
             $stmt->execute(['cpf' => $cpf]);
-
             return json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
         } catch (PDOException $exc) {
             return $exc->getMessage();
         }
     }
 
-    function removeFromList($cpf) {
+    /**
+     * Removes CPF from blacklist setting its status to zero
+     * @param $cpf
+     * @return bool|string
+     */
+    function removeCpfFromList($cpf) {
         try {
             $query = "UPDATE blacklist SET status = 0, datahora_alteracao = datetime('now', 'localtime') WHERE cpf = :cpf AND status = 1";
             $stmt = $this->conn->prepare($query);
@@ -68,7 +89,12 @@ class BlacklistModel
         }
     }
 
-    function addToList($cpf) {
+    /**
+     * Adds CPF to blacklist
+     * @param $cpf
+     * @return int|string
+     */
+    function addCpfToList($cpf) {
         try {
             $query = "INSERT INTO blacklist (cpf, datahora_inclusao) VALUES (:cpf, datetime('now', 'localtime'));";
             $stmt = $this->conn->prepare($query);
